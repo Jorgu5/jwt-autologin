253e85c80b27fc14a21763d680836a893f9bb9b5
Tomek <tomek@sobolew.ski>
2022-06-16 12:41:54 +0200

    init

---
+++ includes/class-jwt-autologin-activator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin activation
+ *
+ * @link       https://sobolew.ski
+ * @since      1.0.0
+ *
+ * @package    Jwt_Autologin
+ * @subpackage Jwt_Autologin/includes
+ */
+
+/**
+ * Fired during plugin activation.
+ *
+ * This class defines all code necessary to run during the plugin's activation.
+ *
+ * @since      1.0.0
+ * @package    Jwt_Autologin
+ * @subpackage Jwt_Autologin/includes
+ * @author     Codeable - Sobolew.ski <tomek@sobolew.ski>
+ */
+class Jwt_Autologin_Activator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function activate() {
+
+	}
+
+}
---
+++ includes/class-jwt-autologin-deactivator.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Fired during plugin deactivation
+ *
+ * @link       https://sobolew.ski
+ * @since      1.0.0
+ *
+ * @package    Jwt_Autologin
+ * @subpackage Jwt_Autologin/includes
+ */
+
+/**
+ * Fired during plugin deactivation.
+ *
+ * This class defines all code necessary to run during the plugin's deactivation.
+ *
+ * @since      1.0.0
+ * @package    Jwt_Autologin
+ * @subpackage Jwt_Autologin/includes
+ * @author     Codeable - Sobolew.ski <tomek@sobolew.ski>
+ */
+class Jwt_Autologin_Deactivator {
+
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since    1.0.0
+	 */
+	public static function deactivate() {
+
+	}
+
+}
